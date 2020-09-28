54568fa0986d26024987fb61d8b83451605f3736
evgeniy_liubich <lyubitch89@yandex.by>
2020-09-29 00:41:33 +0300

    models, views

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'django_movie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
 
-# Create your views here.
+from movies.models import Movie
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
+    # queryset = Movie.objects.get(url=slug)
+    # template_name = 'movies/movie_detail.html'
+    # def get(self, request, slug):
+    #     movie =
+    #     return render(request, , {'movie': movie})
